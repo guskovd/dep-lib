44a4f2c778733dfd3d46ab08d5cf9a3dd70b5977
Danil Guskov <d.guskov@corp.mail.ru>
2017-06-29 16:50:41 +0300

    setup py

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Qapi setup.py"""
+
+from distutils.core import setup
+
+setup(
+    name='Qapi',
+    version='0.1',
+    author='i.am',
+    author_email='e.am@mail.ru',
+    packages=['dep-lib'],
+    url='',
+    download_url='',
+    license='',
+    keywords='dep-lib',
+    description='',
+    install_requires=['requests']
+)
